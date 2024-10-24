76a648c248d64d646b69b29563a80a7240036c49
Yulia Bizunova <yulia320455@gmail.com>
2024-10-24 18:02:02 +0100

    Temp result which work

---
+++ src/main/java/onlinebank/Extractors/UserExtractor.java
@@ -0,0 +1,53 @@
+package onlinebank.Extractors;
+
+import onlinebank.models.*;
+
+import org.springframework.jdbc.core.ResultSetExtractor;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+public class UserExtractor implements ResultSetExtractor<List<User>> {
+
+    @Override
+    public List<User> extractData(ResultSet rs) throws SQLException {
+        List<User> users = new ArrayList<>();
+        while (rs.next()) {
+            User user = new User();
+            user.setName(rs.getString("name"));
+            user.setSurname(rs.getString("surname"));
+            user.setDateOfBirth(rs.getDate("dateOfBirth").toLocalDate());
+            user.setSex(rs.getString("sex"));
+
+
+            // Извлечение номера паспорта
+            user.setPassportNumber(rs.getInt("passportnumber"));
+
+            // Извлекаем списки ипотек и автокредитов из JSONB
+            String mortgageJson = rs.getString("mortgagelist");
+            String autoLoanJson = rs.getString("autoloanlist");
+
+            // Преобразование JSON в список объектов
+            List<Mortgage> mortgages = parseMortgages(mortgageJson);
+            List<AutoLoan> autoLoans = parseAutoLoans(autoLoanJson);
+
+            user.setMortgageList((ArrayList<Mortgage>) mortgages);
+            user.setAutoLoanList((ArrayList<AutoLoan>) autoLoans);
+
+            users.add(user);
+        }
+        return users;
+    }
+
+    private List<Mortgage> parseMortgages(String json) {
+        List<Mortgage> mortgages = new ArrayList<>();
+
+        return mortgages;
+    }
+
+    private List<AutoLoan> parseAutoLoans(String json) {
+        List<AutoLoan> autoLoans = new ArrayList<>();
+
+        return autoLoans;
+    }
+}

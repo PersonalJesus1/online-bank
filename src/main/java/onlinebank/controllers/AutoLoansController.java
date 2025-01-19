9b52536fe4f2d8d868a87ab5edfc65f08030249a
Yulia Bizunova <yulia320455@gmail.com>
2025-01-19 09:32:01 +0000

    change to hibernate, add tranfer money from card to card, deposit to card, deposit to mortgage/autoloan, add file Readme.md

--- src/main/java/onlinebank/controllers/AutoLoansController.java
+++ src/main/java/onlinebank/controllers/AutoLoansController.java
@@ -2,6 +2,7 @@ package onlinebank.controllers;
 
 import jakarta.validation.Valid;
 import onlinebank.models.AutoLoan;
+import onlinebank.models.AutoloanPayment;
 import onlinebank.services.AutoLoanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -32,14 +33,20 @@ public class AutoLoansController {
     }
 
     @PostMapping
-    public String create(@ModelAttribute("autoloan") @Valid AutoLoan autoloan, BindingResult bindingResult) {
+    public String create(@ModelAttribute("autoloan") @Valid AutoLoan autoloan, BindingResult bindingResult, Model model) {
         if (bindingResult.hasErrors()) {
             return "autoloans/newAutoloan";
         }
-        autoLoanService.save(autoloan);
+        try {
+            autoLoanService.save(autoloan);
+        } catch (IllegalArgumentException e) {
+            model.addAttribute("error", e.getMessage());
+            return "autoloans/newAutoloan";
+        }
         return "redirect:/autoloans";
     }
 
+    //edit
     @GetMapping("/{passportNumber}/{mortgageSumm}/edit")
     public String edit(Model model,
                        @PathVariable("passportNumber") int passportNumber,
@@ -49,7 +56,7 @@ public class AutoLoansController {
     }
 
     @PatchMapping("/{passportNumber}/{mortgageSumm}/edit")
-    public String updateMortgage(@ModelAttribute("autoloan") @Valid AutoLoan autoloan, BindingResult bindingResult,
+    public String updateAutoloan(@ModelAttribute("autoloan") @Valid AutoLoan autoloan, BindingResult bindingResult,
                                  @PathVariable("passportNumber") int passportNumber,
                                  @PathVariable("mortgageSumm") double mortgageSumm) {
         if (bindingResult.hasErrors()) {
@@ -59,9 +66,10 @@ public class AutoLoansController {
         return "redirect:/autoloans";
     }
 
-    @GetMapping("/confirm-delete")
-    public String confirmDelete(@RequestParam("passportNumber") int passportNumber,
-                                @RequestParam("mortgageSumm") double mortgageSumm,  Model model) {
+    //delete
+    @GetMapping("/{passportNumber}/{mortgageSumm}/delete")
+    public String confirmDelete(@PathVariable("passportNumber") int passportNumber,
+                                @PathVariable("mortgageSumm") double mortgageSumm, Model model) {
         AutoLoan autoloan = autoLoanService.show(passportNumber, mortgageSumm);
         if (autoloan == null) {
             return "autoloans/not-found";
@@ -70,10 +78,32 @@ public class AutoLoansController {
         return "autoloans/confirm-delete";
     }
 
-    @DeleteMapping("/{passportNumber}/{mortgageSumm}/delete")
+    @PostMapping("/{passportNumber}/{mortgageSumm}/delete")
     public String delete(@PathVariable("passportNumber") int passportNumber,
                          @PathVariable("mortgageSumm") double mortgageSumm) {
         autoLoanService.delete(passportNumber, mortgageSumm);
         return "redirect:/autoloans";
+    }
+
+    //payment
+    @GetMapping("/{id}/payment")
+    public String showPaymentPage(@PathVariable("id") Long id, Model model) {
+        AutoloanPayment autoloanPayment = new AutoloanPayment();
+        autoloanPayment.setId(id);
+        model.addAttribute("autoloanPayment", autoloanPayment);
+        return "autoloans/payment";
+    }
+
+    @PatchMapping("/{id}/payment")
+    public String processPayment(@ModelAttribute("autoloanPayment") AutoloanPayment autoloanPayment,
+                                 @PathVariable("id") Long id, Model model) {
+        try {
+            autoloanPayment.setId(id);
+            autoLoanService.makePayment(autoloanPayment);
+            return "redirect:/autoloans";
+        } catch (IllegalArgumentException e) {
+            model.addAttribute("error", e.getMessage());
+            return "autoloans/payment";
+        }
     }
 }
